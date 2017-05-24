9307de77a4117eb922f5de13d8cdb230865f38e5
tckastanek <tckastanek@gmail.com>
2017-05-24 14:53:10 -0400

    initial file setup

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const {BlogPosts} = require('./models');
+
+const app = express();
+
+const blogPostsRouter = require('./routes/blogposts.js');
+
+app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT  || 3000, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
+});
