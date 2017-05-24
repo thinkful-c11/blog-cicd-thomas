53ac4f20dab4482df636bf5876a0b2b7ca785359
tckastanek <tckastanek@gmail.com>
2017-05-24 15:28:14 -0400

    POST is complete

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -1,11 +1,31 @@
+'use strict';
 const express = require('express');
-const router = express.Router();
-
+const bodyParser = require('body-parser');
 const {BlogPosts} = require('../models');
 
-//
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+
+
 router.get('/', (req, res) => {
   res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
 });
 
 
