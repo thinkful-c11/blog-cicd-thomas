6e68e59d26178210817d128f6fc1684f62afca13
tckastanek <tckastanek@gmail.com>
2017-05-26 14:47:59 -0400

    GET test working

--- test/test-server.js
+++ test/test-server.js
@@ -17,7 +17,23 @@ describe('Blog Posts', function() {
     return closeServer();
   });
 
-
   // Do some its
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+
+        const expectedKeys = ['title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
 
 });
